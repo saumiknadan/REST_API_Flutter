83db6b0fd8d39f159b729b9c3b0b629bdb49d4dc
Saumik Chowdhury <47342920+saumiknadan@users.noreply.github.com>
2023-06-13 23:46:08 +0600

    List from complex JSON without model

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rest_api_flutter/example_four.dart';
 import 'package:rest_api_flutter/example_three.dart';
 import 'package:rest_api_flutter/example_two.dart';
 import 'home_screen.dart';
@@ -18,6 +19,6 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.green,
         ),
-        home: const ExampleThree());
+        home: const ExampleFour());
   }
 }
